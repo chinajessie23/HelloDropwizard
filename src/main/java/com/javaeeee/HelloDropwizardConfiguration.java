69f9433b565f8c2a48248ae95454c095d2f23290
chinajessie23 <jiasichen@yahoo.com>
2016-03-05 16:15:26 -0500

    this is the initial commit!And I amend it using git commit -amend
     with '#' will be ignored, and an empty message aborts the commit.

---
+++ src/main/java/com/javaeeee/HelloDropwizardConfiguration.java
@@ -0,0 +1,12 @@
+package com.javaeeee;
+
+import io.dropwizard.Configuration;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.hibernate.validator.constraints.*;
+import javax.validation.constraints.*;
+
+public class HelloDropwizardConfiguration extends Configuration {
+    // TODO: implement service configuration
+
+
+}
